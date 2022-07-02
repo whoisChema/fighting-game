0afcff5a074019ee28a43e6310084b86cc875c47
Jose Maria Osorio <jose.97.chema@gmail.com>
2022-07-02 00:16:22 -0500

    Healt Bar Interface

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ class Sprite {
             height: 50
         }
         this.color = color
-        this.isAttacking = false
+        this.isAttacking
+        this.health = 100
     }
 
     draw(){
@@ -158,6 +159,8 @@ function animate(){
         player.isAttacking
         ){
         player.isAttacking = false
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
         console.log('attack player')
     }
 
@@ -169,6 +172,8 @@ function animate(){
         enemy.isAttacking
         ){
         enemy.isAttacking = false
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
         console.log('attack enemy')
     }
 }
